1df535a9fdf7e188fde6ed35ba70fe28322a7bf7
fredLeeJH <fred54755475@gmail.com>
2024-04-25 21:56:39 +0900

    [Rename] 패키지 명 변경

---
+++ 프레드/app/src/main/java/com/example/myapplication/BannerFragment.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentBannerBinding
+
+
+class BannerFragment : Fragment() {
+    lateinit var binding: FragmentBannerBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBannerBinding.inflate(inflater,container,false)
+
+        return binding.root
+    }
+}
