148bdaa9c47561374024f80914faf7ea32887e49
starshape77 <111961655+starshape7@users.noreply.github.com>
2024-05-10 17:21:25 +0900

    FEAT
    5주차 과제

--- 우연_추연우/app/src/main/java/com/example/a6th_android/album/AlbumFragment.kt
+++ 우연_추연우/app/src/main/java/com/example/a6th_android/album/AlbumFragment.kt
@@ -3,14 +3,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
-import com.example.a6th_android.home.HomeFragment
 import com.example.a6th_android.MainActivity
 import com.example.a6th_android.R
-import com.example.a6th_android.databinding.FragmentAlbumBinding
 import com.example.a6th_android.album.AlbumVPAdapter
+import com.example.a6th_android.databinding.FragmentAlbumBinding
+import com.example.a6th_android.home.HomeFragment
 import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
@@ -26,10 +25,10 @@ class AlbumFragment : Fragment() {
     ): View? {
         binding = FragmentAlbumBinding.inflate(inflater, container, false)
 
-        setFragmentResultListener("TitleInfo") { requestKey, bundle ->
+        setFragmentResultListener("Title") { requestKey, bundle ->
             binding.albumMusicTitleTv.text = bundle.getString("title")
         }
-        setFragmentResultListener("SingerInfo") { requestKey, bundle ->
+        setFragmentResultListener("Singer") { requestKey, bundle ->
             binding.albumSingerNameTv.text = bundle.getString("singer")
         }
 
